0b3a10af6e066b5b309525bffca381c7f2b62328
Stanlo06-arch <Stanlo06@web.de>
2026-06-05 20:01:46 +0200

    Add configuration for channel ID, role ID, logo, and banner

--- config/ids.js
+++ config/ids.js
@@ -1,1 +1,13 @@
+module.exports = {
 
+  PANEL_CHANNEL_ID: "1498441200062169159",
+
+  SUPPORTER_ROLE_ID: "1497953436514255089",
+
+  LOGO:
+    "https://cdn.discordapp.com/attachments/1475610426766262333/1495199546035146822/Top_Gear.png",
+
+  BANNER:
+    "https://cdn.discordapp.com/attachments/1475610426766262333/1496968229585944676/ChatGPT_Image_23._Apr._2026_20_49_03.png"
+
+};
