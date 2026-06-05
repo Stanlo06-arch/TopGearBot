d007085ed47f9fde66a4fc31c22aab96d6eba4c1
Stanlo06-arch <Stanlo06@web.de>
2026-06-05 07:57:51 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const {
+  Client,
+  GatewayIntentBits,
+  Partials
+} = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ],
+  partials: [Partials.Channel]
+});
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} online`);
+});
+
+client.login(process.env.TOKEN);
