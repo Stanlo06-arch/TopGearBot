3d17a727d0f0834935d16380a39b0fa267e85490
Stanlo06-arch <Stanlo06@web.de>
2026-08-19 22:13:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,30 +19,18 @@ const client = new Client({
 
 });
 
-// ================================
-// SYSTEME
-// ================================
-
 require('./systems/panel')(client);
 require('./systems/stance')(client);
 require('./systems/xenon')(client);
 require('./systems/urlaub')(client);
 require('./systems/sanktion')(client);
 
-// ================================
-// READY
-// ================================
-
 client.once('ready', () => {
 
   console.log(
-    `✅ ${client.user.tag} online`
+    `${client.user.tag} online`
   );
 
 });
-
-// ================================
-// LOGIN
-// ================================
 
 client.login(process.env.TOKEN);
